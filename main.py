2520919012e50126f8696a7d24590d2c5053dc2d
ChayaleF <152266797+ChayaleF@users.noreply.github.com>
2024-03-08 00:29:16 +0200

    server

--- main.py
+++ main.py
@@ -1,34 +1,68 @@
-import sys
-from functools import lru_cache
-import datetime
-import time
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, constr, ValidationError, validator, field_validator
 
-def timein(func):
-    def inner():
-        timestart=time.time()
-        func()
-        print(time.time()-timestart)
-    return inner()
-
-def print_hi(name):
-    print(f'Hi, {name}')
-
-@timein
-def formil():
-    for i in range(1000000):
-        print(i)
+app = FastAPI()
 
 
-@lru_cache()
-def fibonacci(n):
-    if n < 2:
-        return n
-    else:
-        return fibonacci(n - 2) + fibonacci(n - 1)
+class Task(BaseModel):
+    name: str
+    description: str
+    id: int
+    status: bool
 
-print([fibonacci(x) for x in range(1222)])
-def print_hi(name):
-    print(f'Hi, {name}')
+    @field_validator('id')
+    def check_id(cls, id):
+        task = [t for t in tasks if t["id"] == id]
+        if  task==None:
+            raise ValueError('error')
+        return id
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    @field_validator('name')
+    def check_name(cls, id):
+        task = [t for t in tasks if t["id"] == id]
+        if len(task["name"]) == 0:
+            raise ValueError('error')
+        return id
+
+tasks = [{"name":"sara","description":"ertu","id":5,"status":False}]
+
+@app.get("/{id}")
+async def get_task(id:int):
+    task = [t for t in tasks if t["id"] == id]
+    return task
+
+
+@app.post("/task/")
+async def add_task(task: Task):
+    try:
+        tasks.append(task.dict())
+    except ValidationError:
+        raise HTTPException(status_code=400, detail="oops... an error occurred")
+    return f"Hello {task.name}"
+
+
+@app.put("/{id}", response_model=Task)
+async def update_task(newTask: Task,id:int):
+    try:
+        updated=jsonable_encoder(newTask)
+        task = [t for t in tasks if t["id"] == id]
+        task= updated
+    except ValidationError:
+        raise HTTPException(status_code=400, detail="oops... an error occurred")
+    return updated
+
+@app.delete("/{id}")
+async def delete_task(id:int):
+    try:
+        task = [t for t in tasks if t["id"] == id]
+        deleted=task
+        del task
+    except ValidationError:
+        raise HTTPException(status_code=400, detail="oops... an error occurred")
+    return deleted
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8080)
